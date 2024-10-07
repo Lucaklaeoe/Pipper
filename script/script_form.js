38373e3c7c014eb41954224cbcc2526dff9f14cf
Emil <emilloengaa@icloud.com>
2024-10-07 14:26:26 +0200

    css change

--- script/script_form.js
+++ script/script_form.js
@@ -29,3 +29,31 @@ const icon = document.getElementById("material-symbols-outlined");
             dropdownContent.style.display = "flex";
         }
     }
+
+    document.getElementById("form_new_post").addEventListener("submit", async (event) => {
+        event.preventDefault();
+    
+        const name = document.getElementById("name").value;
+        const content = document.getElementById("content").value;
+        
+        const data = {
+            name: name,
+            content: content,
+    
+        };
+        const url = "http://localhost:8000/newpip";
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+        
+        const response = await fetch(url, options)
+        const cat = await response.json();
+        
+        console.log(newpiper_data);
+        console.log("Det fucking virker!");
+        
+    })
